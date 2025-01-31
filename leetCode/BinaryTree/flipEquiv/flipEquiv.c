31e24c81486363c079418526327525b31f133be2
yoraji <rajiy8299@gmail.com>
2025-01-31 16:27:32 +0100

    flipEquiv

---
+++ leetCode/BinaryTree/flipEquiv/flipEquiv.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+bool flipEquiv(struct TreeNode* root1, struct TreeNode* root2) {
+	if(root1 == NULL && root2 == NULL)
+		return true;
+	if(root1 == NULL || root2 == NULL)
+		return false;
+	if(root1->val != root2->val)
+		return false;
+	return(flipEquiv(root1->left, root2->right) && flipEquiv(root1->right, root2->left) ||
+			flipEquiv(root1->right, root2->right) && flipEquiv(root1->left, root2->left));
+}
