54ed7da19d13e6161706411ee68fecfdafe3f987
youssef raji <youssef@darkness.darkness>
2025-07-21 21:23:03 +0100

    findMaxConsecutiveOnes

--- leetCode/Likeit/findMaxConsecutiveOnes/findMaxConsecutiveOnes.c
+++ leetCode/Likeit/findMaxConsecutiveOnes/findMaxConsecutiveOnes.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <limits.h>
 
 int findMaxConsecutiveOnes(int* nums, int numsSize) {
     int nm = INT_MIN;
@@ -16,4 +17,11 @@ int findMaxConsecutiveOnes(int* nums, int numsSize) {
     if(result >= nm)
         nm = result;
     return nm;
+}
+int main() {
+    int nums[] = {1, 1, 0, 1, 1, 1};
+    int size = sizeof(nums) / sizeof(nums[0]);
+    int maxConsecutiveOnes = findMaxConsecutiveOnes(nums, size);
+    printf("Max consecutive ones: %d\n", maxConsecutiveOnes);
+    return 0;
 }
